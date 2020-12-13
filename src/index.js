eed8f6560554e3a15a77bfbec5365b9fdc65252c
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-12-13 09:34:14 -0300

    testando a criação de multiplo bundles

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import printMe from "./print";
+
 const box = document.getElementById("box");
+const btn = document.createElement("button");
+
 box.innerHTML = "Hello World";
+btn.innerHTML = "Click me and check the console!";
+btn.onclick = printMe;
+box.appendChild(btn);
