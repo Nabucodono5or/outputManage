3e86a996f94dfaa58ad770022e79724984465021
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-12-15 10:53:36 -0300

    usando middleware com node, usando webpack server, náo usando watch

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,13 @@ module.exports = {
     print: "./src/print.js",
   },
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
@@ -18,5 +23,6 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
 };
