111e55e8b6fdd52105678c12fe2c5f4960629fa3
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-12-15 08:57:45 -0300

    clean dist folder e mode development com devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: "inline-source-map",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
